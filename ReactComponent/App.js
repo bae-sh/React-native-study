a04fa27ea5ac0e78f07faeefc90844dc9633fe5c
bae-sh <b5460881@naver.com>
2021-07-21 14:16:49 +0900

    todoApp

--- ReactComponent/App.js
+++ ReactComponent/App.js
@@ -6,38 +6,73 @@ import {
   Button,
   TouchableOpacity,
   TextPropTypes,
+  TextInput,
+  Pressable,
 } from 'react-native';
+import styled from 'styled-components';
 
-function App() {
+const App = () => {
   const name = 'bae';
   return (
-    <View style={styles.container}>
-      {/* <Button
-          title="버튼"
-          onPress={() => {
-            alert('Click!!');
-          }}
-        /> */}
-      <Count></Count>
+    <View>
+      {/* <Count></Count>
+      <EventInput></EventInput>
+      <Button2></Button2>
+
+      <View style={textStyle.container}>
+        <Text style={[textStyle.text, {color: 'green'}]}>text</Text>
+        <Text style={textStyle.error}>erorr</Text>
+      </View> */}
+      <View style={[styles.container, styles.header]}></View>
+      <View style={[styles.container, styles.contents]}></View>
+      <View style={[styles.container, styles.footer]}></View>
+    </View>
+  );
+};
+const ButtonContainer = styled.TouchableOpacity`
+  background-color: #9b59b6;
+  border-radius: 15px;
+  padding: 15px 40px;
+  margin: 10px 0px;
+  justify-content: center;
+`;
+
+const Title = styled.Text`
+  font-size: 20px;
+  font-weight: 600;
+  color: #fff;
+`;
+const Button2 = props => {
+  return (
+    <Pressable style={{backgroundColor: 'green', width: 200}}>
+      <Text>123</Text>
+    </Pressable>
+  );
+};
+function EventInput(props) {
+  let [text, setText] = useState('2');
+  return (
+    <View>
+      <TextInput
+        style={{borderWidth: 1, padding: 10, fontSize: 20}}
+        placeholder="Enter a text.."
+        onChange={e => {
+          setText(e.nativeEvent.text);
+        }}></TextInput>
     </View>
   );
 }
 
 function MyButton(props) {
   return (
-    <Button
-      title={props.title}
-      style={{
-        backgroundColor: '#3498db',
-        padding: 16,
-        margin: 10,
-        borderRadius: 8,
-      }}
-      onPress={() => {
-        props.onPress();
-      }}></Button>
+    <TouchableOpacity
+      style={{backgroundColor: 'blue', margin: 10, padding: 16}}
+      onPress={() => props.onPress()}>
+      <Text style={{color: 'white', fontSize: 24}}>{props.title}</Text>
+    </TouchableOpacity>
   );
 }
+
 function Count() {
   let [count, setCount] = useState(0);
 
@@ -58,12 +93,40 @@ function Count() {
     </View>
   );
 }
-const styles = StyleSheet.create({
+
+let styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    width: '100%',
+    backgroundColor: 'gray',
     alignItems: 'center',
     justifyContent: 'center',
+    height: 80,
+  },
+  header: {
+    height: 80,
+    backgroundColor: 'red',
+  },
+  contents: {
+    backgroundColor: 'yellow',
+    height: 320,
+  },
+  footer: {
+    height: 80,
+    backgroundColor: 'green',
+  },
+});
+let textStyle = StyleSheet.create({
+  text: {
+    padding: 10,
+    fontSize: 26,
+    fontWeight: '600',
+    color: 'black',
+    backgroundColor: 'yellow',
+    margin: 20,
+  },
+  error: {
+    fontWeight: '400',
+    color: 'red',
   },
 });
 export default App;

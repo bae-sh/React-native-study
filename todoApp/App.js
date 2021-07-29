5b1dd7651ac53de7b1182d4a247386786e03c6f9
bae-sh <b5460881@naver.com>
2021-07-29 19:46:18 +0900

    Task

--- todoApp/App.js
+++ todoApp/App.js
@@ -1,47 +1,7 @@
-import React, {useState} from 'react';
-import {View, Text, StatusBar} from 'react-native';
-import styled, {ThemeProvider} from 'styled-components';
-import {theme} from './theme';
-import Input from './Input';
-const Container = styled.SafeAreaView`
-  flex: 1;
-  background-color: ${({theme}) => theme.background};
-  align-items: center;
-  justify-content: flex-start;
-`;
-const Title = styled.Text`
-  font-size: 40px;
-  font-weight: 600;
-  color: ${({theme}) => theme.main};
-  align-self: flex-start;
-  margin: 0px 20px;
-`;
+import React from 'react';
+import Main from './src/App';
 const App = () => {
-  const [newTask, setNewTask] = useState('');
-  const _addTask = () => {
-    alert('Add:' + {newTask});
-    setNewTask('');
-  };
-  const _handleTextChange = text => {
-    setNewTask(text);
-  };
-  return (
-    <ThemeProvider theme={theme}>
-      <Container>
-        <StatusBar
-          barStyle="light-content"
-          backgroundColor={theme.background}
-        />
-        <Title>TODO List</Title>
-        <Input
-          placeholder="+ Add a task"
-          value={newTask}
-          onChangeText={_handleTextChange}
-          onSubmitEditing={_addTask}
-        />
-      </Container>
-    </ThemeProvider>
-  );
+  return <Main />;
 };
 
 export default App;
